7c8e5685ba88da9bcc8c8c62060d3242f1c27f33
Augusto <argus3d@gmail.com>
2025-08-19 15:50:33 -0300

    Delete jogos/obfuscate.sh

--- jogos/obfuscate.sh
+++
@@ -1,9 +0,0 @@
-# Example: obfuscate_recursive.sh
-#!/bin/bash
-
-# Find all JavaScript files and obfuscate them
-find . -type f -name "*.js" -exec bash -c '
-  for file do
-    javascript-obfuscator "$file" --output "$file"
-  done
-' _ {} +
